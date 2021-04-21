5cfe71c1eb6cf29ab7468c1ceb4d61fa8621f231
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-04-21 17:20:24 +0530

    Refactor Flip Box App | block-BJaaep | Assignment: Flip Box

--- block-BJaaep/code/script.js
+++ block-BJaaep/code/script.js
@@ -1,14 +1,4 @@
 
-let wrapperOne = document.querySelector(".wrapper1");
-let wrapperTwo = document.querySelector(".wrapper2");
-
-// selcect all boxes of wrapper one
-let allBoxesOfWrapperOne = wrapperOne.querySelectorAll(".box");
-
-// selcect all boxes of wrapper two
-let allBoxesOfWrapperTwo = wrapperTwo.querySelectorAll(".box");
-
-
 
 // Genrate random number from 1 to 12.
 
@@ -23,52 +13,56 @@ function getRandomColor() {
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-// console.log(getRandomNumber(1, 12));
+// // console.log(getRandomNumber(1, 12));
 
-// create EventHandler for Wrapper 1 With event delegation
+ // create EventHandler for Wrapper 1 Without event delegation
 
+let firstBoxes = document.querySelectorAll(".first li");
 
-    allBoxesOfWrapperOne.forEach(box => {
-        box.addEventListener("click", function() {
-            allBoxesOfWrapperOne.forEach(box => {
+firstBoxes.forEach((box)  => {
+    box.addEventListener("click", (event) => {
+        let box = event.target;
+        firstBoxes.forEach(box => {
                 box.innerText = "";
                 box.style.background = "#444";
                 box.style.transform = "none";
-            });
-            box.style.background = getRandomColor();
-            box.innerText = getRandomNumber(1, 12);
-            box.style.color = getRandomColor();
-            box.style.transform = "rotateY(360deg)";
-            box.style.transition = " 1s ease-in-out"; 
-            setTimeout(()=> {
-                box.innerText = "";
-                box.style.transform = "none";
-                box.style.background = "#444";
-            },5000 )            
         });
+
+        box.style.background = getRandomColor();
+        box.innerText = getRandomNumber(1, 12);
+        box.style.color = getRandomColor();
+        box.style.transform = "rotateX(360deg)";
+        box.style.transition = " 1s ease-in-out"; 
+        setTimeout(() => {
+            box.innerText = "";
+            box.style.transform = "none";
+            box.style.background = "#444";
+        }, 5000);
+    })
+});
+
+ // Create Eventhandler of Wrapper 2 with event Delegation
+
+let secondBox = document.querySelector(".second");
+let secondBoxes = document.querySelectorAll(".second li");
+
+secondBox.addEventListener("click", (event) => {
+    let box = event.target;
+    secondBoxes.forEach(box => {
+        box.innerText = "";
+        box.style.background = "#444";
+        box.style.transform = "none";
     });
 
+    box.innerText = getRandomNumber(1, 12);
+    box.style.background = getRandomColor();
+    box.style.color = getRandomColor();
+    box.style.transform = "rotateY(360deg)";
+    box.style.transition = " 1s ease-in-out"; 
 
-// Create Eventhandler of Wrapper 2 without event Delegation
-
-wrapperTwo.addEventListener("click", function()  {
-    allBoxesOfWrapperTwo.forEach(box => {
-        box.addEventListener("click", function() {
-            allBoxesOfWrapperTwo.forEach(box => {
-                box.innerText = "";
-                box.style.background = "#444";
-                box.style.transform = "none";
-            });
-            box.style.background = getRandomColor();
-            box.innerText = getRandomNumber(1, 12);
-            box.style.color = getRandomColor();
-            box.style.transform = "rotateX(360deg)";
-            box.style.transition = " 1s ease-in-out"; 
-            setTimeout(()=> {
-                box.innerText = "";
-                box.style.transform = "none";
-                box.style.background = "#444";
-            },5000 );            
-        });
-    });
+    setTimeout(() => {
+        box.innerText = "";
+        box.style.transform = "none";
+        box.style.background = "#444";
+    }, 5000);
 });
